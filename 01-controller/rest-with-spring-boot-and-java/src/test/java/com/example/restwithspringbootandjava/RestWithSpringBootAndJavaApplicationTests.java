d5279038d07b1665c93fcfa148f4bab70059c656
Yohan Silva <yohanps.dev@gmail.com>
2023-03-29 10:38:02 -0300

    feat: create customized exceptions

---
+++ 01-controller/rest-with-spring-boot-and-java/src/test/java/com/example/restwithspringbootandjava/RestWithSpringBootAndJavaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.restwithspringbootandjava;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertEquals(1,2);
+	}
+
+}
