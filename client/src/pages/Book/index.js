03d4f8fa94f6e1d6100e36d1670d022cce6e35aa
Yohan Silva <yohanps.dev@gmail.com>
2023-08-04 18:58:19 -0300

    style: add style to book list

--- client/src/pages/Book/index.js
+++ client/src/pages/Book/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FiPower } from "react-icons/fi";
+import { FiPower, FiEdit, FiTrash2 } from "react-icons/fi";
 
 import "./styles.css";
 
@@ -18,9 +18,55 @@ export default function Book() {
           Add new book
         </Link>
         <button type="button">
-            <FiPower size={18} color="#251FC5"/>
+          <FiPower size={18} color="#251FC5" />
         </button>
       </header>
+
+      <h1>Registered Books</h1>
+      <ul>
+        <li>
+          <strong>Title:</strong>
+          <p>Docker Deep Dive</p>
+          <strong>Author:</strong>
+          <p>Nigel Poulton</p>
+          <strong>Price:</strong>
+          <p>R$ 47,90</p>
+          <strong>Release Date:</strong>
+          <p>17/07/2017</p>
+
+          <button type="button">
+            <FiEdit size={20} color="#251FC5" />
+          </button>
+        </li>
+        <li>
+          <strong>Title:</strong>
+          <p>Docker Deep Dive</p>
+          <strong>Author:</strong>
+          <p>Nigel Poulton</p>
+          <strong>Price:</strong>
+          <p>R$ 47,90</p>
+          <strong>Release Date:</strong>
+          <p>17/07/2017</p>
+
+          <button type="button">
+            <FiEdit size={20} color="#251FC5" />
+          </button>
+        </li>
+        <li>
+          <strong>Title:</strong>
+          <p>Docker Deep Dive</p>
+          <strong>Author:</strong>
+          <p>Nigel Poulton</p>
+          <strong>Price:</strong>
+          <p>R$ 47,90</p>
+          <strong>Release Date:</strong>
+          <p>17/07/2017</p>
+
+          <button type="button">
+            <FiEdit size={20} color="#251FC5" />
+          </button>
+        </li>
+      </ul>
     </div>
   );
 }
