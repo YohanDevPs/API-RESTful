54616d8c1b8a73dd325330d2383a322f7532bab7
Yohan Silva <yohanps.dev@gmail.com>
2023-08-04 18:43:23 -0300

    feat: implement header of books

---
+++ client/src/pages/Book/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiPower } from "react-icons/fi";
+
+import "./styles.css";
+
+import logoImage from "../../assets/logo.svg";
+
+export default function Book() {
+  return (
+    <div className="book-container">
+      <header>
+        <img src={logoImage} />
+        <span>
+          Welcome, <strong>Leandro</strong>!
+        </span>
+        <Link className="button" to="book/new">
+          Add new book
+        </Link>
+        <button type="button">
+            <FiPower size={18} color="#251FC5"/>
+        </button>
+      </header>
+    </div>
+  );
+}
