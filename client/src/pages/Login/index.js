33af922f0035da04b790ac22c60933f9cd9c5318
Yohan Silva <yohanps.dev@gmail.com>
2023-08-08 14:25:12 -0300

    feat: integrate login client to api

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,19 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./styles.css";
 
 import logoImage from "../../assets/logo.svg";
 import padlock from "../../assets/padlock.png";
+import api from "../../services/api";
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate(); 
+
+  async function login(event) {
+    event.preventDefault();
+
+    const data = {
+      username,
+      password,
+    };
+
+    try {
+      const response = await api.post("/auth/signin", data);
+
+      localStorage.setItem("username", username);
+      localStorage.setItem("accessToken", response.data.token);
+
+      navigate("/books"); 
+    } catch (err) {
+      console.log("Erro - ", err);
+      alert("Login failed, Try again");
+    }
+  }
+
   return (
     <div className="login-container">
       <section className="form">
         <img src={logoImage} alt="Logo" />
-        <form action="">
+        <form onSubmit={login}>
           <h1>Access your Account</h1>
-          <input placeholder="Username" />
-          <input type="password" placeholder="Password" />
-          <button className="button" type="submit">Login</button>
+          <input
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="button" type="submit">
+            Login
+          </button>
         </form>
       </section>
       <img src={padlock} alt="Login" />
